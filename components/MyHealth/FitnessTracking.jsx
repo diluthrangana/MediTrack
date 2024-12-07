fafc92d0a5ab7eba0962272b0c84c613aebe89f1
Diluth Rangana <diluthrangana@gmail.com>
2024-12-08 02:03:51 +0530

    workout done

--- components/MyHealth/FitnessTracking.jsx
+++ components/MyHealth/FitnessTracking.jsx
@@ -1,51 +1,103 @@
-import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
-import React, { useContext } from 'react';
+import React, { useContext, useState, useEffect } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  ScrollView,
+  Image,
+} from "react-native";
 import { useRouter } from "expo-router";
-import MediDataContext from '../../context/MediDataContext';
+import MediDataContext from "../../context/MediDataContext";
+import WorkoutData from "../../data/WorkoutPlans";
 
 export default function FitnessTracking() {
   const router = useRouter();
   const { userData } = useContext(MediDataContext);
+  const [bmiCategory, setBMICategory] = useState([]);
+  const [currentDayWorkout, setCurrentDayWorkout] = useState([]);
+
+  useEffect(() => {
+    // Calculate BMI and determine category
+    const calculateBMI = (weight, height) => {
+      const heightInMeters = height / 100; // Convert cm to meters
+      return (weight / (heightInMeters * heightInMeters)).toFixed(2);
+    };
+
+    if (userData?.weight && userData?.height) {
+      const bmi = calculateBMI(userData.weight, userData.height);
+
+      // Determine BMI category
+      if (bmi < 18.5) setBMICategory(WorkoutData["Underweight"]);
+      else if (bmi >= 18.5 && bmi < 24.9)
+        setBMICategory(WorkoutData["Healthy Weight"]);
+      else if (bmi >= 25 && bmi < 29.9)
+        setBMICategory(WorkoutData["Overweight"]);
+      else if (bmi >= 30) setBMICategory(WorkoutData["Obese"]);
+    }
+  }, [userData]);
+
+  useEffect(() => {
+    // Determine current day's workout plan dynamically
+    const getCurrentDayWorkout = () => {
+      const today = new Date().getDay(); // Get the current day of the week
+      console.log("Today's index:", today);
+      console.log("Today's workouts from BMI Category:", bmiCategory[today]);
+      setCurrentDayWorkout(bmiCategory[today]?.workouts || []);
+    };
+
+    getCurrentDayWorkout();
+  }, [bmiCategory]);
 
   return (
-    <View
-      style={styles.button}
-      onPress={() => router.push('editmyhealth/EditMedication')}
-    >   
+    <View style={styles.button}>
       <View style={styles.container}>
         {/* Vertical Card */}
-        <View style={styles.verticalcard}>
-        </View>
+        <View style={styles.verticalcard}></View>
 
         {/* First Horizontal Card */}
         <View style={styles.horizontalcard1}>
-          <ScrollView 
-            horizontal 
-            showsHorizontalScrollIndicator={false} 
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
             style={styles.carousel}
           >
-            {/* Example Images */}
-            <Image source={{ uri: 'https://via.placeholder.com/100' }} style={styles.carouselImage} />
-            <Image source={{ uri: 'https://via.placeholder.com/100' }} style={styles.carouselImage} />
-            <Image source={{ uri: 'https://via.placeholder.com/100' }} style={styles.carouselImage} />
-            <Image source={{ uri: 'https://via.placeholder.com/100' }} style={styles.carouselImage} />
-            <Image source={{ uri: 'https://via.placeholder.com/100' }} style={styles.carouselImage} />
+            <Image
+              source={{ uri: "https://via.placeholder.com/100" }}
+              style={styles.carouselImage}
+            />
+            <Image
+              source={{ uri: "https://via.placeholder.com/100" }}
+              style={styles.carouselImage}
+            />
+            <Image
+              source={{ uri: "https://via.placeholder.com/100" }}
+              style={styles.carouselImage}
+            />
           </ScrollView>
         </View>
 
-        {/* Second Horizontal Card */}
+        {/* Second Horizontal Card - Dynamically Updated with Today's Workout */}
         <View style={styles.horizontalcard2}>
-        <ScrollView 
-            horizontal 
-            showsHorizontalScrollIndicator={false} 
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
             style={styles.carousel}
           >
-            {/* Example Images */}
-            <Image source={require('./../../assets/images/WorkOut/lunge.jpg')} style={styles.carouselImage} />
-            <Image source={require('./../../assets/images/WorkOut/bridge.jpg')} style={styles.carouselImage} />
-            <Image source={require('./../../assets/images/WorkOut/plank.jpg')} style={styles.carouselImage} />
-            <Image source={require('./../../assets/images/WorkOut/jumping jack.jpg')} style={styles.carouselImage} />
-            <Image source={require('./../../assets/images/WorkOut/lunge.jpg')} style={styles.carouselImage} />
+            {currentDayWorkout?.map((item, index) => (
+              <View key={index} style={styles.workoutContainer}>
+                <Image
+                  source={item.image} // Fallback to placeholder
+                  style={styles.image}
+                />
+                {/* <Text style={styles.workoutText}>{item.exercise}</Text>
+                <Text style={styles.workoutDetails}>
+                  {item.sets
+                    ? `${item.sets} sets x ${item.reps || ""} reps`
+                    : ""}
+                </Text> */}
+              </View>
+            ))}
           </ScrollView>
         </View>
       </View>
@@ -95,11 +147,9 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  carousel: {
-    
-  },
+  carousel: {},
   carouselImage: {
-    width: 70, // Square image
+    width: 70,
     height: 70,
     borderRadius: 10,
     marginRight: 10,
@@ -121,4 +171,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  image: {
+    width: 70,
+    height: 70,
+    borderRadius: 10,
+    marginRight: 10,
+  }
 });
+ 
+
---
+++ data/WorkoutPlans.jsx
@@ -0,0 +1,223 @@
+const WorkoutData = {
+    "Underweight": [
+        {
+          "day": "Day 1 - Strength & Core",
+          "workouts": [
+            { "exercise": "Bridge", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/bridge.jpg') },
+            { "exercise": "Flutter Kicks", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/flutter_kicks.jpg') },
+            { "exercise": "Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/lunges.jpg') },
+            { "exercise": "Mountain Climbers", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/mountain_climbers.jpg') }
+          ]
+        },
+        {
+          "day": "Day 2 - Upper Body",
+          "workouts": [
+            { "exercise": "Push-ups", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/pushups.jpg') },
+            { "exercise": "Plank Shoulder Taps", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/plank_taps.jpg') },
+            { "exercise": "Tricep Dips", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/tricep_dips.jpg') },
+            { "exercise": "Bicep Curls (Bodyweight)", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/bicep_curls.jpg') }
+          ]
+        },
+        {
+          "day": "Day 3 - Lower Body",
+          "workouts": [
+            { "exercise": "Squats", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/squats.jpg') },
+            { "exercise": "Step-ups (Stairs)", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/stepups.jpg') },
+            { "exercise": "Glute Bridges", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/glute_bridges.jpg') },
+            { "exercise": "Calf Raises", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/calf_raises.jpg') }
+          ]
+        },
+        {
+          "day": "Day 4 - Cardio & Core",
+          "workouts": [
+            { "exercise": "Jumping Jacks", "sets": 3, "reps": 30, "image": require('../assets/images/WorkOut/jumping_jacks.jpg') },
+            { "exercise": "High Knees", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/high_knees.jpg') },
+            { "exercise": "Sit-ups", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/situps.jpg') },
+            { "exercise": "Bicycle Crunches", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/bicycle_crunches.jpg') }
+          ]
+        },
+        {
+          "day": "Day 5 - Full Body",
+          "workouts": [
+            { "exercise": "Burpees", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Push-ups", "sets": 3, "reps": 10, "image": require('../assets/images/WorkOut/pushups.jpg') },
+            { "exercise": "Plank Hold", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/plank_hold.jpg') },
+            { "exercise": "Jump Squats", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/jump_squats.jpg') }
+          ]
+        },
+        {
+          "day": "Day 6 - Active Recovery",
+          "workouts": [
+            { "exercise": "Yoga - Downward Dog", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/downward_dog.jpg') },
+            { "exercise": "Child's Pose", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/childs_pose.jpg') },
+            { "exercise": "Cat Cow Stretch", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/cat_cow_stretch.jpg') },
+            { "exercise": "Side Stretch", "sets": 3, "reps": "20 sec each side", "image": require('../assets/images/WorkOut/side_stretch.jpg') }
+          ]
+        },
+        {
+          "day": "Day 7 - Cardio & Agility",
+          "workouts": [
+            { "exercise": "Burpees", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Skaters", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/skaters.jpg') },
+            { "exercise": "Lateral Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/lateral_lunges.jpg') },
+            { "exercise": "Jump Rope (Imaginary)", "sets": 3, "reps": 30, "image": require('../assets/images/WorkOut/jump_rope.jpg') }
+          ]
+        }
+      ],
+      "Healthy Weight": [
+        {
+          "day": "Day 1 - Strength & Core",
+          "workouts": [
+            { "exercise": "Bridge", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/bridge.jpg') },
+            { "exercise": "Flutter Kicks", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/flutter_kicks.jpg') },
+            { "exercise": "Mountain Climbers", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/mountain_climbers.jpg') },
+            { "exercise": "Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/lunges.jpg') }
+          ]
+        },
+        {
+          "day": "Day 2 - Cardio Blast",
+          "workouts": [
+            { "exercise": "Burpees", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Jumping Jacks", "sets": 3, "reps": 30, "image": require('../assets/images/WorkOut/jumping_jacks.jpg') },
+            { "exercise": "High Knees", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/high_knees.jpg') },
+            { "exercise": "Side-to-Side Hops", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/side_hops.jpg') }
+          ]
+        },
+        {
+          "day": "Day 3 - Upper Body",
+          "workouts": [
+            { "exercise": "Push-ups", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/pushups.jpg') },
+            { "exercise": "Plank Shoulder Taps", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/plank_taps.jpg') },
+            { "exercise": "Wall Push-ups", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/wall_pushups.jpg') },
+            { "exercise": "Tricep Dips", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/tricep_dips.jpg') }
+          ]
+        },
+        {
+          "day": "Day 4 - Active Recovery",
+          "workouts": [
+            { "exercise": "Yoga - Downward Dog", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/downward_dog.jpg') },
+            { "exercise": "Child's Pose", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/childs_pose.jpg') },
+            { "exercise": "Cat Cow Stretch", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/cat_cow_stretch.jpg') },
+            { "exercise": "Side Stretch", "sets": 3, "reps": "20 sec each side", "image": require('../assets/images/WorkOut/side_stretch.jpg') }
+          ]
+        },
+        {
+          "day": "Day 5 - Cardio & Agility",
+          "workouts": [
+            { "exercise": "Skaters", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/skaters.jpg') },
+            { "exercise": "Lateral Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/lateral_lunges.jpg') },
+            { "exercise": "Burpees", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Mountain Climbers", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/mountain_climbers.jpg') }
+          ]
+        },
+        {
+          "day": "Day 6 - Full Body",
+          "workouts": [
+            { "exercise": "Plank Hold", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/plank_hold.jpg') },
+            { "exercise": "Burpees", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Step-ups (Stairs)", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/stepups.jpg') },
+            { "exercise": "Jump Squats", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/jump_squats.jpg') }
+          ]
+        },
+        {
+          "day": "Day 7 - Agility & Conditioning",
+          "workouts": [
+            { "exercise": "Jump Rope (Imaginary)", "sets": 3, "reps": 30, "image": require('../assets/images/WorkOut/jump_rope.jpg') },
+            { "exercise": "Side Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/side_lunges.jpg') },
+            { "exercise": "Squat Jumps", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/jump_squats.jpg') },
+            { "exercise": "Burpees", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/burpees.jpg') }
+          ]
+        }
+      ],
+      "Overweight": [
+        {
+          "day": "Day 1 - Strength & Core",
+          "workouts": [
+            { "exercise": "Bridge", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/bridge.jpg') },
+            { "exercise": "Flutter Kicks", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/flutter_kicks.jpg') },
+            { "exercise": "Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/lunges.jpg') },
+            { "exercise": "Side Plank", "sets": 3, "reps": "30 sec each side", "image": require('../assets/images/WorkOut/side_plank.jpg') }
+          ]
+        },
+        {
+          "day": "Day 2 - Cardio Blast",
+          "workouts": [
+            { "exercise": "Burpees", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Jumping Jacks", "sets": 3, "reps": 30, "image": require('../assets/images/WorkOut/jumping_jacks.jpg') },
+            { "exercise": "High Knees", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/high_knees.jpg') },
+            { "exercise": "Mountain Climbers", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/mountain_climbers.jpg') }
+          ]
+        },
+        {
+          "day": "Day 3 - Upper Body",
+          "workouts": [
+            { "exercise": "Push-ups (Wall)", "sets": 3, "reps": 10, "image": require('../assets/images/WorkOut/wall_pushups.jpg') },
+            { "exercise": "Plank Shoulder Taps", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/plank_taps.jpg') },
+            { "exercise": "Tricep Dips", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/tricep_dips.jpg') },
+            { "exercise": "Wall Press", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/wall_pushups.jpg') }
+          ]
+        },
+        {
+          "day": "Day 4 - Active Recovery",
+          "workouts": [
+            { "exercise": "Cat Cow Stretch", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/cat_cow_stretch.jpg') },
+            { "exercise": "Child's Pose", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/childs_pose.jpg') },
+            { "exercise": "Side Stretch", "sets": 3, "reps": "20 sec each side", "image": require('../assets/images/WorkOut/side_stretch.jpg') },
+            { "exercise": "Yoga - Downward Dog", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/downward_dog.jpg') }
+          ]
+        },
+        {
+          "day": "Day 5 - Cardio & Agility",
+          "workouts": [
+            { "exercise": "Skaters", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/skaters.jpg') },
+            { "exercise": "Side Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/side_lunges.jpg') },
+            { "exercise": "Burpees", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Jump Squats", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/jump_squats.jpg') }
+          ]
+        },
+        {
+          "day": "Day 6 - Full Body",
+          "workouts": [
+            { "exercise": "Plank Hold", "sets": 3, "reps": "30 sec", "image": require('../assets/images/WorkOut/plank_hold.jpg') },
+            { "exercise": "Burpees", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/burpees.jpg') },
+            { "exercise": "Mountain Climbers", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/mountain_climbers.jpg') },
+            { "exercise": "Step-ups (Stairs)", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/stepups.jpg') }
+          ]
+        },
+        {
+          "day": "Day 7 - Agility & Conditioning",
+          "workouts": [
+            { "exercise": "Jump Rope (Imaginary)", "sets": 3, "reps": 30, "image": require('../assets/images/WorkOut/jump_rope.jpg') },
+            { "exercise": "Side Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/side_lunges.jpg') },
+            { "exercise": "Squat Jumps", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/jump_squat.jpg') },
+            { "exercise": "Burpees", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/burpees.jpg') }
+          ]
+        }
+      ],
+      "Obese": [
+    {
+      "day": "Day 1 - Strength & Core",
+      "workouts": [
+        { "exercise": "Chair Squats", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/chair_squats.jpg') },
+        { "exercise": "Wall Push-ups", "sets": 3, "reps": 10, "image": require('../assets/images/WorkOut/wall_pushups.jpg') },
+        { "exercise": "Flutter Kicks", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/flutter_kicks.jpg') },
+        { "exercise": "Lunges", "sets": 3, "reps": 12, "image": require('../assets/images/WorkOut/lunges.jpg') }
+      ]
+    },
+    {
+      "day": "Day 2 - Cardio Blast",
+      "workouts": [
+        { "exercise": "Burpees", "sets": 3, "reps": 10, "image": require('../assets/images/WorkOut/burpees.jpg') },
+        { "exercise": "Mountain Climbers", "sets": 3, "reps": 15, "image": require('../assets/images/WorkOut/mountain_climbers.jpg') },
+        { "exercise": "Jumping Jacks", "sets": 3, "reps": 25, "image": require('../assets/images/WorkOut/jumping_jacks.jpg') },
+        { "exercise": "Side Hops", "sets": 3, "reps": 20, "image": require('../assets/images/WorkOut/side_hops.jpg') }
+      ]
+    }
+  ]
+    
+  
+  }
+  
+  
+
+  export default WorkoutData;
